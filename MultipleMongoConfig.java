98c8724678e3ba0df5cb77978cb8b43b8e566059
unknown <gsravanreddy4@gmail.com>
2021-05-11 01:48:26 +0530

    changes

--- MultipleMongoConfig.java
+++ MultipleMongoConfig.java
@@ -58,6 +58,7 @@ public class MultipleMongoConfig {
     @Primary
     public MongoDbFactory primaryFactory(final MongoProperties mongo) throws Exception {
         MongoCollection<PrimaryModel> mongoCollections = new MongoClient().getDatabase("").getCollection("", PrimaryModel.class);
+
         Morphia morphia = new Morphia();
         morphia.mapPackage("com.baeldung.morphia");
         Datastore datastore = morphia.createDatastore(new MongoClient(), "library");
@@ -65,36 +66,89 @@ public class MultipleMongoConfig {
         String clientId = "";
         String memberId= "";
         String startDate= "";
+
         List<PrimaryModel> primaryModels = new ArrayList<>();
+
         datastore.createQuery(PrimaryModel.class).field("clientId").equal("").field("memberId").equal("").field("start_date").equal("").field("created_date").lessThanOrEq("").field("last_modified_date").equal("");
         Query<PrimaryModel> primaryModelQuery = datastore.createQuery(PrimaryModel.class);
-        addEqualCriteria("client_id",clientId, primaryModelQuery);
-        addEqualCriteria("member_id",clientId, primaryModelQuery);
-        addEqualCriteria("start_date",clientId, primaryModelQuery);
-        addEqualCriteria("client_id",clientId, primaryModelQuery);
-        addGTEAndLTECriteria("created_date",new Date(),new Date(),primaryModelQuery);
-        addGTEAndLTECriteria("last_modified_date",new Date(),new Date(),primaryModelQuery);
-        addEqualCriteria("member.firstName",clientId, primaryModelQuery);
-        addEqualCriteria("member.lastName",clientId, primaryModelQuery);
-        addEqualCriteria("member.dob",clientId, primaryModelQuery);
+        Bson clientFilter = addEqualCriteria("client_id",clientId);
+        addEqualCriteria("memberId",clientId);
+        addEqualCriteria("startDate",clientId);
+        addEqualCriteria("clientId",clientId);
+        Bson createdDateFilter = addGTEAndLTECriteria("createdDate",new Date(),new Date());
+        Bson lastModifiedDate =  addGTEAndLTECriteria("lastModifiedDate",new Date(),new Date());
+        addEqualCriteria("member.firstName",clientId);
+        addEqualCriteria("member.lastName",clientId);
+        addEqualCriteria("member.dob",clientId);
         List<PrimaryModel> primaryModelList = primaryModelQuery.find().toList();
         return new SimpleMongoDbFactory(new MongoClient(mongo.getHost(), mongo.getPort()),
                 mongo.getDatabase());
     }
 
-    private void addEqualCriteria(String fieldName,String fieldValue, Query<PrimaryModel> primaryModelQuery) {
-        if(StringUtils.isEmpty(fieldValue)){
-            primaryModelQuery.field(fieldName).equal(fieldValue);
+
+    public List<MedicalEligibility> getQueryResults(MedicalEligibility medicalEligibility){
+        MongoCollection<MedicalEligibility> medicalEligibilityCollection = new MongoClient().getDatabase("").getCollection("", MedicalEligibility.class);
+        if(medicalEligibility != null){
+            List<Bson> bsons= new ArrayList<>();
+            if(medicalEligibility.getClientId() != null){
+            bsons.add(addEqualCriteria("clientId",medicalEligibility.getClientId()));
+            }
+            if(medicalEligibility.getMemberId() != null){
+                bsons.add(addEqualCriteria("memberId",medicalEligibility.getMemberId()));
+            }
+            if(medicalEligibility.getMember().getFirstName() != null){
+                bsons.add(addEqualCriteria("member.firstName",medicalEligibility.getMember().getFirstName()));
+            }
+            if(medicalEligibility.getMember().getLastName() != null){
+                bsons.add(addEqualCriteria("member.firstName",medicalEligibility.getMember().getLastName()));
+            }
+            if(medicalEligibility.getMember().getLastName() != null){
+                bsons.add(addEqualCriteria("member.dob",medicalEligibility.getMember().getDob()));
+            }
+            return medicalEligibilityCollection.find(Filters.and(bsons)).into(new ArrayList<>());
         }
+        return null;
     }
 
-    private void addGTEAndLTECriteria(String fieldName,Date lessDate,Date greaterDate, Query<PrimaryModel> primaryModelQuery) {
+    public List<MedicalEligibility> getMedicalEligilibilityByCreatedDateAndClientId(String clientId,Date startDate,Date endDate){
+        MongoCollection<MedicalEligibility> medicalEligibilityCollection = new MongoClient().getDatabase("").getCollection("", MedicalEligibility.class);
+        List<Bson> bsons = new ArrayList<>();
+        if(clientId != null){
+            bsons.add(addEqualCriteria("clientId",clientId));
+        }
+        bsons.add(addGTEAndLTECriteria("createdDate",startDate,endDate));
+        return medicalEligibilityCollection.find(Filters.and(bsons)).into(new ArrayList<>());
+    }
+
+    public List<MedicalEligibility> getMedicalEligilibilityByLastModifiedDateAndClientId(String clientId,Date startDate,Date endDate){
+        MongoCollection<MedicalEligibility> medicalEligibilityCollection = new MongoClient().getDatabase("").getCollection("", MedicalEligibility.class);
+        List<Bson> bsons = new ArrayList<>();
+        if(clientId != null){
+            bsons.add(addEqualCriteria("clientId",clientId));
+        }
+        bsons.add(addGTEAndLTECriteria("lastModifiedDate",startDate,endDate));
+        return medicalEligibilityCollection.find(Filters.and(bsons)).into(new ArrayList<>());
+    }
+
+
+    private Bson addEqualCriteria(String fieldName,String fieldValue) {
+        if(StringUtils.isEmpty(fieldValue)){
+            return Filters.eq(fieldName,fieldValue);
+        }
+        return null;
+    }
+
+    private Bson addGTEAndLTECriteria(String fieldName,Date lessDate,Date greaterDate) {
+        Bson greaterThanFilter = null;
         if(lessDate != null){
-            primaryModelQuery.field(fieldName).greaterThanOrEq(lessDate);
+            greaterThanFilter = Filters.gte(fieldName,lessDate);
         }
+        Bson lesserThanFilter = null;
         if(greaterDate != null){
-            primaryModelQuery.field(fieldName).lessThanOrEq(greaterDate);
+            greaterThanFilter = Filters.gte(fieldName,lessDate);
         }
+        Bson andBsonFilter = Filters.and(greaterThanFilter,lesserThanFilter);
+        return andBsonFilter;
     }
 
     @Bean
